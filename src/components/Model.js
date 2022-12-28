7d6d75583c04380093689ddfd1ace822bdee4ade
virgiawankusuma <virgiawan150@gmail.com>
2022-12-28 10:35:06 +0700

    delete not used func and syntax

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,36 +1,12 @@
 /* eslint-disable no-unused-vars */
 import '@google/model-viewer';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { 
   FaCamera,
-  FaVolumeMute,
-  FaVolumeUp,
    
 } from 'react-icons/fa';
 
 export default function Model() {
-  const [model, setModel ] = useState(null);
-  const [audio, setAudio] = useState(null);
-  const [isMuted, setIsMuted] = useState(false);
-  
-  useEffect(() => {
-    setModel(document.querySelector('#model'));
-    setAudio(document.querySelector('#audio'));
-  }, []);
-
-  const linkHandler = (e, url) => {
-    e.preventDefault();
-    window.open(url, '_blank');
-  }
-
-  // const PlayAudio = () => {
-  //   audio.play();
-  // }
-
-  const MuteAudio = () => {
-    audio.muted = !audio.muted;
-    setIsMuted(!isMuted);
-  }
 
   return (
     <model-viewer
@@ -48,24 +24,12 @@ export default function Model() {
       autoplay
     >
       <button 
-        onClick={() => MuteAudio()}
-        className="absolute flex gap-2 items-center bottom-4 left-4 p-2 border-[1px] border-white rounded-full bg-white shadow shadow-secondary hover:shadow-lg hover:shadow-secondary text-primary transition-all duration-150"
-      >
-        {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
-      </button>
-      <button 
         slot="ar-button"
-        // onClick={() => PlayAudio()}
         className="absolute flex gap-2 items-center bottom-4 right-4 py-1 px-3 border-[1px] border-secondary rounded-full bg-secondary shadow shadow-secondary hover:shadow-lg hover:shadow-secondary text-white transition-all duration-150"
       >
         <span>Enable AR</span>
         <FaCamera />
       </button>
-      {/* <audio 
-        id="audio"
-      >
-        <source src="./audio/Hudson-balanced.mp3" type="audio/mpeg"></source>
-      </audio> */}
     </model-viewer>
   )
 }
