a69ec24c28e2b5c61fb0af8d45d69b97dbf3c573
virgiawankusuma <virgiawan150@gmail.com>
2022-12-28 11:41:48 +0700

    create Groups data

---
+++ src/utils/data.js
@@ -0,0 +1,177 @@
+const Groups = () => ([
+  {
+    id: 0,
+    title: 'AESPA',
+    url: 'https://digidaxa.com/viewar/aespa',
+    // ini adalah aespa
+    randstr: '$2y$10$hyY/icm4upbPYQEyZD7Eyuei13tqSa1/yX8tDOLBFzfa5A450nGFC',
+    colorPalette: {
+      primary: '#FF0000',
+      secondary: '#000000',
+      tertiary: '#FFFFFF',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/aespa.glb',
+      iosSrc: '',
+      alt: 'A 3D model of AESPA'
+    }
+  },
+  {
+    id: 1,
+    title: 'BLACKPINK',
+    url: 'https://digidaxa.com/viewar/blackpink',
+    // ini adalah blackpink
+    randstr: '$2y$10$ak1vW3JelvLNfYKwdO/8ue9nbKU5dDkKCC96lJ6hyqIQiUVUd9DJu',
+    colorPalette: {
+      primary: '#FF69B4',
+      secondary: '#000000',
+      tertiary: '#FFFFFF',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/blackpink.glb',
+      iosSrc: '',
+      alt: 'A 3D model of BLACKPINK'
+    }
+  },
+  {
+    id: 2,
+    title: 'BTS',
+    url: 'https://digidaxa.com/viewar/bts',
+    // ini adalah bts
+    randstr: '$2y$10$jePYMdqqrXN2.n2SXjvhyOEXK4gyXJVl7JCXln9SLtHUYBtDghdHm',
+    colorPalette: {
+      primary: '#6C2DC7',
+      secondary: '#FF0000',
+      tertiary: '#FFFFFF',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/bts.glb',
+      iosSrc: '',
+      alt: 'A 3D model of BTS'
+    }
+  },
+  {
+    id: 3,
+    title: 'ITZY',
+    url: 'https://digidaxa.com/viewar/itzy',
+    // ini adalah itzy
+    randstr: '$2y$10$gHqtRYGB413yV6LlUBnvkehiXEl6sa6u4bXzv7PJYGyDKmneZNOWG',
+    colorPalette: {
+      primary: '#6C2DC7',
+      secondary: '#FF0000',
+      tertiary: '#FFFF00',
+      quaternary: '#00FF00',
+      quinary: '#',
+    },
+    model: {
+      src: './models/itzy.glb',
+      iosSrc: '',
+      alt: 'A 3D model of ITZY'
+    }
+  },
+  {
+    id: 4,
+    title: 'NCT DREAM',
+    url: 'https://digidaxa.com/viewar/nctdream',
+    // ini adalah nct dream
+    randstr: '$2y$10$gHqtRYGB413yV6LlUBnvkehiXEl6sa6u4bXzv7PJYGyDKmneZNOWG',
+    colorPalette: {
+      primary: '#87CEFA',
+      secondary: '#FFFFFF',
+      tertiary: '#',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/nctdream.glb',
+      iosSrc: '',
+      alt: 'A 3D model of NCT DREAM'
+    }
+  },
+  {
+    id: 5,
+    title: 'RED VELVET',
+    url: 'https://digidaxa.com/viewar/redvelvet',
+    // ini adalah red velvet
+    randstr: '$2y$10$XzrdfAahuQ30ihFJZi/8lOD6/OWEhWKFjBO12pkyd2dwtgfrmMO8q',
+    colorPalette: {
+      primary: '#FF0000',
+      secondary: '#FFFFFF',
+      tertiary: '#',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/redvelvet.glb',
+      iosSrc: '',
+      alt: 'A 3D model of RED VELVET'
+    }
+  },
+  {
+    id: 6,
+    title: 'SEVENTEEN',
+    url: 'https://digidaxa.com/viewar/seventeen',
+    // ini adalah seventeen
+    randstr: '$2y$10$ipO2023UNTdGPtzh5APPAOGBjiwgo8to2XVQEr7BVuKrPhgyeTdl.',
+    colorPalette: {
+      primary: '#FF66CC',
+      secondary: '#FFFFFF',
+      tertiary: '#',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/seventeen.glb',
+      iosSrc: '',
+      alt: 'A 3D model of SEVENTEEN'
+    }
+  },
+  {
+    id: 7,
+    title: 'Stray Kids',
+    url: 'https://digidaxa.com/viewar/straykids',
+    // ini adalah stray kids
+    randstr: '$2y$10$ltyjicIqzDl0Md85oYb5C.p8/bTFW4w79z7PMvX9pY3m/.eGezRm2',
+    colorPalette: {
+      primary: '#000000',
+      secondary: '#FFFFFF',
+      tertiary: '#',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/straykids.glb',
+      iosSrc: '',
+      alt: 'A 3D model of Stray Kids'
+    }
+  },
+  {
+    id: 8,
+    title: 'Tomorrow X Together',
+    url: 'https://digidaxa.com/viewar/txt',
+    // ini adalah txt
+    randstr: '$2y$10$PlhlpXAu5M53tdbogQ/gz.ByfDScZpfqiFvuuNhH.FTRFu1Pp9nSe',
+    colorPalette: {
+      primary: '#6C2DC7',
+      secondary: '#FFFFFF',
+      tertiary: '#',
+      quaternary: '#',
+      quinary: '#',
+    },
+    model: {
+      src: './models/txt.glb',
+      iosSrc: '',
+      alt: 'A 3D model of Tomorrow X Together'
+    }
+  },
+]);
+
+export { 
+  Groups
+};
